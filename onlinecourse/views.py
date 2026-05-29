edf85c872dbd7465ad1d18781d9a86f895511a5b
itsSonchan <sonja.grund@icloud.com>
2026-05-29 15:17:24 -0400

    fixed some typos and added results to exam_result template

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -109,7 +109,7 @@ def submit(request, course_id):
     submission=Submission.objects.create(enrollment=enrollment)
     choices=extract_answers(request)
     submission.choices.set(choices)
-    submission_id=submission_id
+    submission_id=submission.id
     return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id,submission_id)))
     
     return HttpResponseRedirect(reverse('onlinecourse:show_exam_result'))
@@ -131,12 +131,12 @@ def show_exam_result(request, course_id, submission_id):
     submission=Submission.objects.get(id=submission_id)
     choices=submission.choices.all()
     total_score=0
-    questions=course.choice_set.all()
+    questions=course.question_set.all()
     for question in questions:
         correct_choices=question.choice_set.filter(is_correct=True)
         selected_choices=choices.filter(question=question)
          
-         if set(correct_choices)==set(selected_choices):
+        if set(correct_choices)==set(selected_choices):
             total_score+=question.grade
     context['course']=course
     context['grade']=total_score
